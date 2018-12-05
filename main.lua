7d0bb0f6efbad2ca8785cf6619d68c0c4e8ff9be
HaNaK0 <saturn-v@hotmail.com>
2018-12-06 00:06:17 +0100

    Added some gound work for breakpoint debugging

--- main.lua
+++ main.lua
@@ -1,8 +1,28 @@
 --cold hand
 --copyright Hampus Huledal
 
-function love.load(args)
+function love.load(args, uargs)
+    print("Cold Hand started \n copyright Hampus Huledal")
+    print("Args: ")
+    --parse args
+    --TODO: add better parsing taking arguments and checking for more
+    local parsedArgs = {}
+    for index, value in ipairs(args) 
+    do
+        print(index .. ": " .. value)
+        parsedArgs[value] = true;  
+    end
+    
+    --This is if you are using visual studio code and want to debug your code using the Lua Debugger(https://marketplace.visualstudio.com/items?itemName=devCAT.lua-debug).
+    if parsedArgs["-VSCodeDebug"] 
+    then
+        local json = require 'Json.dkjson'
+        local debuggee = require 'Debugging.vscode-debuggee'
+        local startResult, breakerType = debuggee.start(json)
+        print('debuggee start ->', startResult, breakerType)
+    end
 
+    print("testing debug")
 end
 
 function love.update(deltaTime)
