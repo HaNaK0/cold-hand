85f4c3602929d176d25cfc6aa1666b493e2ccfd9
HaNaK0 <saturn-v@hotmail.com>
2019-02-23 17:47:29 +0100

    added globals

---
+++ globals.lua
@@ -0,0 +1,7 @@
+--globals
+--don't put too much things here. It's dangerous
+local miscUtils = assert(require("Util.miscUtil"), "could not find Util.miscUtil.lua")
+
+globals = {}
+
+globals.Utils = miscUtils
