caa7f94040d3fc3edc2da9f371f5ecd8fdbbd1a8
HaNaK0 <saturn-v@hotmail.com>
2019-03-05 21:07:36 +0100

    Created a samll wrapper for xml2lua

--- External/XML/xml.lua
+++ External/XML/xml.lua
@@ -1,4 +1,18 @@
-local xml2lua = require("xml2lua-master.xml2lua")
-local handler = require("xml2lua-master.xmlhandler.tree")
+--A small wrapper for xml2lua
+--Written by Hampus Huledal
 
-return xml2lua.parser(handler)
+local xml2lua = globals.Utils.AssertRequire("xml2lua-master.xml2lua")
+local xmlTree = globals.Utils.AssertRequire("xml2lua-master.xmlhandler.tree")
+
+local wrapper = {}
+
+function wrapper.parseToTree(path)
+    assert(path, "path is nil, should be a string")
+
+    local handlerTree = xmlTree:new()
+    local parser = xml2lua.parser(handlerTree)
+    parser:parse(xml2lua.loadFile(path))
+    return handlerTree
+end
+
+return wrapper
